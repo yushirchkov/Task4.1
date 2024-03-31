34d4b91a74f3ea3020ec5264de4d783ab6ed9095
Юрок <yushirchkov@gmail.com>
2024-03-31 19:39:12 +0300

    исправил ошибки

--- backend-resources/src/test/java/com/itm/space/backendresources/controller/UserControllerTest.java
+++ backend-resources/src/test/java/com/itm/space/backendresources/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.itm.space.backendresources.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itm.space.backendresources.BaseIntegrationTest;
 import com.itm.space.backendresources.api.request.UserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -20,19 +22,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-
 @SpringBootTest
-@Testcontainers
 @AutoConfigureMockMvc
-class UserControllerTest {
+class UserControllerTest extends BaseIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Container
     private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.5");
 
     @DynamicPropertySource
@@ -46,8 +44,9 @@ class UserControllerTest {
     @WithMockUser(roles = "MODERATOR")
     public void createUserTest() throws Exception {
         // Given
-        UserRequest userRequest = new UserRequest("CatUser", "cat.doe@example.com", "test",
-                "Cat", "Cattin");
+        UserRequest userRequest = new UserRequest("UserFan", "cat.doe@example.com", "test",
+                "Fan", "Cattin");
+
         // When
         mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -55,6 +54,21 @@ class UserControllerTest {
                 // Then
                 .andExpect(status().isOk());
     }
+    @Test
+    @WithMockUser(roles = "MODERATOR")
+    public void createUserTest_InvalidEmail() throws Exception {
+        // Given
+        UserRequest userRequest = new UserRequest("UserFan", "invalid-email", "test", "Fan", "Cattin");
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(userRequest)))
+
+                // Then
+                .andExpect(status().isBadRequest());
+    }
+
 
     @Test
     @WithMockUser(roles = "MODERATOR")
@@ -65,25 +79,14 @@ class UserControllerTest {
         // when
         mockMvc.perform(MockMvcRequestBuilders.get("/api/users/{id}", userID))
                 // then
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON),
-                        content().json("""
-                                    {
-                                       "firstName": "",
-                                       "lastName": "",
-                                       "email": "kit@mail.com",
-                                       "roles": [
-                                         "default-roles-itm"
-                                       ],
-                                       "groups": [
-                                         "Moderators"
-                                       ]
-                                     }
-                                """)
-                );
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value(""))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value(""))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(""))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value("default-roles-itm"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.groups").value("Moderators"));
     }
-
     @Test
     @WithMockUser(username = "testUser", roles = "MODERATOR")
     void helloIntegrationTest() throws Exception {
@@ -92,7 +95,7 @@ class UserControllerTest {
         // When
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/users/hello")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andReturn();
         // Then
         String responseContent = result.getResponse().getContentAsString();
@@ -106,5 +109,14 @@ class UserControllerTest {
             throw new RuntimeException(e);
         }
     }
+    @Test
+    @WithMockUser(roles = "MODERATOR")
+    void getUserById_NonExistingUser() throws Exception {
 
-}
+        var nonExistingUserID = "non-existing-user-id";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/{id}", nonExistingUserID))
+
+                .andExpect(status().isNotFound());
+
+}}
